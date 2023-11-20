9dfd4b18e107992b7c125ab1972f871ca523aef9
Jon Smith <jjsmith.okc@gmail.com>
2023-11-19 21:09:06 -0600

    Add profile pic

---
+++ src/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div className='home-container'>
+            <h1>Welcome to Katie Paints Homes!</h1>
+            <img src="/profile.jpeg" alt="profile" />
+            <p>This is some of my work</p>
+        </div>
+    );
+};
+
+export default Home;
